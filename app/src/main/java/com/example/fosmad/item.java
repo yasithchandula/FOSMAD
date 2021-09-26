c0f6a9481a651dc22dd7663aab1d65b7630ed8f6
yasithchandula <76646376+yasithchandula@users.noreply.github.com>
2021-09-26 13:06:25 +0530

    item model update

--- app/src/main/java/com/example/fosmad/item.java
+++ app/src/main/java/com/example/fosmad/item.java
@@ -1,9 +1,31 @@
 package com.example.fosmad;
 
+import com.google.firebase.database.Exclude;
+
 public class item {
+    private String imageUrl;
     private String name;
     private String price;
-    private String discription;
+    private String category;
+    private String description;
+    @Exclude
+    String itemKey;
+
+    public String getItemKey() {
+        return itemKey;
+    }
+
+    public void setItemKey(String itemKey) {
+        this.itemKey = itemKey;
+    }
+
+    public item(String imageUrl){
+        this.imageUrl=imageUrl;
+    }
+
+    public item(){
+
+    }
 
 
     public String getName() {
@@ -22,11 +44,28 @@ public class item {
         this.price = price;
     }
 
-    public String getDiscription() {
-        return discription;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDiscription(String discription) {
-        this.discription = discription;
+    public void setDescription(String description) {
+        this.description = description;
     }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 }
