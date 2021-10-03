ba0dab7feed50fc61811e60fa365bda73b2d4485
Thushal Shaminda <it20216146@my.sliit.lk>
2021-10-03 12:27:11 +0530

    function update

--- app/src/main/java/com/example/fosmad/MainActivity.java
+++ app/src/main/java/com/example/fosmad/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogOut = findViewById(R.id.btnLogout);
+        //btnLogOut = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
 
         btnLogOut.setOnClickListener(view ->{
